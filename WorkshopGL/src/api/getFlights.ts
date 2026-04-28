22632f15b7e90f257aa5da5cf6a1352481120130
Andrei Bosca <andreibosca200424@gmail.com>
2026-04-28 17:44:59 +0300

    finalFlights

--- WorkshopGL/src/api/getFlights.ts
+++ WorkshopGL/src/api/getFlights.ts
@@ -6,7 +6,7 @@ const API_BASE_URL = "https://localhost:8080";
 
 export const getFlights = async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/flights`);
+        const response = await axios.get(`${API_BASE_URL}/flights/search`);
         const airplaneInfromation: AirplaneInformation[] = response.data;
         console.log("Fetched flights:", airplaneInfromation);
         return airplaneInfromation;
